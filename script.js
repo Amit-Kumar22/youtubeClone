155974bbff629bcc4ba098d47142162af3126045
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-08-03 23:12:13 +0530

    script.js

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
-const videoCardContainer = document.querySelector('.video-container');
-let api_key = "AIzaSyDRzZCNPxbaQl_kMUMt2Dg9SexUjgHLIfM";
-let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+const vedioCardContainer = document.querySelector('.video-container');
+
+let api_key = "AIzaSyAEwL1eIor7nqinmj8SME7BlN6wQbr56pY";
+let vedio_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
-fetch(video_http + new URLSearchParams({
-    key: api_key,
-    part: 'snippet',
-    chart: 'mostPopular',
-    maxResults: 50,
-    regionCode: 'IN'
+
+fetch(vedio_http + new URLSearchParams({
+    key : api_key,
+    part :'snippet',
+    chart : 'mostPopular',
+    maxResults : 75,
+    regionCode : 'IN'
 }))
+
 .then(res => res.json())
 .then(data => {
-    // console.log(data);
-    data.items.forEach(item => {
+   console.log(data);
+      data.items.forEach(item => {
         getChannelIcon(item);
-    })
+      })
 })
 .catch(err => console.log(err));
-const getChannelIcon = (video_data) => {
-    fetch(channel_http + new URLSearchParams({
-        key: api_key,
-        part: 'snippet',
-        id: video_data.snippet.channelId
-    }))
-    .then(res => res.json())
-    .then(data => {
-        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-        makeVideoCard(video_data);
-    })
+
+const getChannelIcon = (vedio_data) => {
+      fetch(channel_http + new URLSearchParams({
+        key : api_key,
+        part :'snippet',
+        id : vedio_data.snippet.channelId
+      }))
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        vedio_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVedioCard(vedio_data);
+      })
 }
-const makeVideoCard = (data) => {
-    videoCardContainer.innerHTML += `
-    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
-        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
-        <div class="content">
-            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
-            <div class="info">
-                <h4 class="title">${data.snippet.title}</h4>
-                <p class="channel-name">${data.snippet.channelTitle}</p>
-            </div>
-        </div>
-    </div>
-    `;
-}
-// search bar
-const searchInput = document.querySelector('.search-bar');
-const searchBtn = document.querySelector('.search-btn');
-let searchLink = "https://www.youtube.com/results?search_query=";
-searchBtn.addEventListener('click', () => {
+
+const makeVedioCard = (data) => {
+    vedioCardContainer.innerHTML += `
+    <div class="vedio" onclick ="location.href = 'https://youtube.com/watch?v=${data.id}'">
+         <img src="${data.snippet.thumbnails.high.url}" heigth = 300px width = 300px class="thumbnail" alt="channel thumbnail">
+           <div class="content">
+              <img src="${data.channelThumbnail}" class="channel-icon" alt="channel icon">
+                  <div class="info">
+                     <h4 class="title">${data.snippet.title}</h4>
+                     <p class="channel-name">${data.snippet.channelTitle}</p>
+                  </div> 
+                 </div>
+                </div>
+                `;
+            }
+
+  const searchInput = document.querySelector('.search-bar');
+  const searchBtn = document.querySelector('.search-btn');
+  let searchLink ="https://www.youtube.com/results?search_query=";    
+  
+  searchBtn.addEventListener('click' , () => {
     if(searchInput.value.length){
         location.href = searchLink + searchInput.value;
     }
-})
+  })
